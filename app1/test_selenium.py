7f2053915b7afce9c0c5f730d10a714ef728f58e
hank-ehly <hank.ehly@smapenergy.com>
2019-12-06 17:09:49 +0900

    Try adding options

--- app1/test_selenium.py
+++ app1/test_selenium.py
@@ -1,12 +1,18 @@
 import unittest
 
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.keys import Keys
 
 
 class PythonOrgSearch(unittest.TestCase):
     def setUp(self):
-        self.driver = webdriver.Chrome()
+        options = Options()
+
+        options.add_argument("--headless")
+        # options.add_argument("--disable-dev-shm-usage")
+
+        self.driver = webdriver.Chrome(chrome_options=options)
 
     def test_search_in_python_org(self):
         self.driver.get("http://www.python.org")
