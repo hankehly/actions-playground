6787b3e309177f4fc066391ebf578c3e39463728
hank-ehly <hank.ehly@smapenergy.com>
2019-12-06 14:53:24 +0900

    Add selenium test to workflow

---
+++ app1/test_selenium.py
@@ -0,0 +1,24 @@
+import unittest
+
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+
+
+class PythonOrgSearch(unittest.TestCase):
+    def setUp(self):
+        self.driver = webdriver.Chrome()
+
+    def test_search_in_python_org(self):
+        self.driver.get("http://www.python.org")
+        self.assertIn("Python", self.driver.title)
+        elem = self.driver.find_element_by_name("q")
+        elem.send_keys("pycon")
+        elem.send_keys(Keys.RETURN)
+        assert "No results found." not in self.driver.page_source
+
+    def tearDown(self):
+        self.driver.close()
+
+
+if __name__ == "__main__":
+    unittest.main()
