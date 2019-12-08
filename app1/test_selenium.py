baf7c1e1c97af7e70dbf4e025983a1faa115894e
Hank Ehly <henry.ehly@gmail.com>
2019-12-08 09:59:15 +0900

    Upload selenium artifacts

--- app1/test_selenium.py
+++ app1/test_selenium.py
@@ -1,18 +1,36 @@
+import os
 import unittest
+from pathlib import Path
 
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.keys import Keys
 
+ARTIFACTS = Path(__file__).parent.parent / "artifacts"
+
 
 class PythonOrgSearch(unittest.TestCase):
+    """
+    An example selenium test case that makes an assertion
+    takes a screenshot and checks runtime environment
+    """
+
+    @classmethod
+    def setUpClass(cls) -> None:
+        Path(ARTIFACTS).mkdir(exist_ok=True)
+
     def setUp(self):
-        options = Options()
-        options.add_argument("--headless")
+        if os.getenv("GITHUB_ACTIONS") or os.getenv("HEADLESS"):
+            options = Options()
+            options.add_argument("--headless")
+        else:
+            options = None
+
         self.driver = webdriver.Chrome(options=options)
 
     def test_search_in_python_org(self):
         self.driver.get("http://www.python.org")
+        self.driver.save_screenshot(str(Path(ARTIFACTS) / "screenshot.png"))
         self.assertIn("Python", self.driver.title)
         elem = self.driver.find_element_by_name("q")
         elem.send_keys("pycon")
