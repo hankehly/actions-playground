be90bbaa8052cf3d0b16a80181bd93fcf46562cb
hank-ehly <hank.ehly@smapenergy.com>
2019-12-09 15:18:21 +0900

    And another

--- .github/actions/poetry/main.js
+++ .github/actions/poetry/main.js
@@ -1,13 +1,13 @@
-import * as core from '@actions/core';
-import { exec } from '@actions/exec';
+const core = require("@actions/core");
+const exec = require("@actions/exec");
 
 
 // most @actions toolkit packages have async methods
 async function run() {
     try {
         // const version = core.getInput('version');
-        await exec('curl -O -sSL https://raw.githubusercontent.com/sdispater/poetry/master/get-poetry.py');
-        await exec('python get-poetry.py --preview');
+        await exec.exec('curl -O -sSL https://raw.githubusercontent.com/sdispater/poetry/master/get-poetry.py');
+        await exec.exec('python get-poetry.py --preview');
         core.addPath(`${process.env.HOME}/.poetry/bin`);
     } catch (error) {
         core.setFailed(error.message);
